e5b6a7ae1dcdec6e712bda8637cfa690dde542a7
janceviciusjulius <janceviciusjulius@gmail.com>
2024-09-30 21:52:58 +0300

    Update

---
+++ src/classif2.py
@@ -0,0 +1,74 @@
+from osgeo import gdal
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+ESTIMATORS = 100
+
+def createGeotiff(outRaster, dataG, transform, proj):
+    driver = gdal.GetDriverByName("GTiff")
+    rowsG, colsG = dataG.shape
+    rasterDS = driver.Create(outRaster, colsG, rowsG, 1, gdal.GDT_Byte)
+    rasterDS.SetGeoTransform(transform)
+    rasterDS.SetProjection(proj)
+    band = rasterDS.GetRasterBand(1)
+    band.WriteArray(dataG)
+    rasterDS = None
+
+inputRaster = "/Users/juliusjancevicius/Desktop/Intelektualios_informacines_sistemos/data/2024-07-09..2024-07-10 0-1%/CLEANED 2024-07-09..2024-07-10 0-1%/2024-07-09..2024-07-10 T35UMA.tiff"
+df = pd.read_csv("/Users/juliusjancevicius/Desktop/Intelektualios_informacines_sistemos/learning_data/training_ground_June.csv", sep=",")
+outputRaster = "demo.tiff"
+data = df[
+    ["S2_2", "S2_3", "S2_4", "S2_5", "S2_6", "S2_7", "S2_8", "S2_8A", "S2_11", "S2_12", "NDTI",	"NDVIre", "MNDWI"]
+]
+label = df["COD"]
+del df
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(data.values, label.values, test_size=0.2, random_state=42)
+
+# Train the model
+clf = RandomForestClassifier(n_estimators=ESTIMATORS)
+print("Training")
+clf.fit(X_train, y_train)
+
+# Predict on the test set
+print("Predicting")
+y_pred_test = clf.predict(X_test)
+
+# Calculate and print accuracy
+accuracy = accuracy_score(y_test, y_pred_test)
+print(f"Accuracy on the test set: {accuracy * 100:.2f}%")
+
+# Continue with the full prediction process on the raster data
+ds = gdal.Open(inputRaster, gdal.GA_ReadOnly)
+rows = ds.RasterYSize
+cols = ds.RasterXSize
+bands = ds.RasterCount
+geo_transform = ds.GetGeoTransform()
+projection = ds.GetProjectionRef()
+array = ds.ReadAsArray().astype("uint16")
+ds = None
+
+array = np.stack(array, axis=2)
+array = np.reshape(array, [rows * cols, bands])
+test = pd.DataFrame(array, dtype="uint16")
+del array
+
+# Predict on the full raster data
+y_pred = clf.predict(test)
+del test
+classification_RF = y_pred.reshape((rows, cols))
+del y_pred
+
+# Save the classified raster
+print("Saving")
+createGeotiff(
+    outRaster=outputRaster,
+    dataG=classification_RF,
+    transform=geo_transform,
+    proj=projection,
+)
+del classification_RF
