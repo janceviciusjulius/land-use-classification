f0a44824db513162960e88dba1c61d217db003d6
janceviciusjulius <janceviciusjulius@gmail.com>
2025-04-16 22:48:00 +0300

    Formating imports

--- src/additional/gdal_merge.py
+++ src/additional/gdal_merge.py
@@ -3,7 +3,6 @@
 import sys
 
 from osgeo.gdal import deprecation_warn
-
 # import osgeo_utils.gdal_merge as a convenience to use as a script
 from osgeo_utils.gdal_merge import *  # noqa
 from osgeo_utils.gdal_merge import main
